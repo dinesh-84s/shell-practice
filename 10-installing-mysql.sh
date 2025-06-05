1feb61b289865c82d2291b6aef312e09f2ce1016
dineshkonda <dineshkonda3239@gmail.com>
2025-06-06 00:14:34 +0530

    hello world started

--- 10-installing-mysql.sh
+++ 10-installing-mysql.sh
@@ -11,6 +11,11 @@ else
     echo "you are running with the root access"
 fi
 
+dnf list installed mysql
+if [ $USERID -ne 0]
+then 
+    echo "Mysql not installaed is going to be.. installed"
+
 dnf install mysql -y
 
 
@@ -21,4 +26,22 @@ else
     echo "Installing Mysql is... FAILURE"
     exit 1
 
+    fi
+  
+    echo "mysql is installed nothing to do"
+
 fi
+
+
+
+# dnf install mysql -y
+
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing Mysql is... SUCCESS"
+# else
+#     echo "Installing Mysql is... FAILURE"
+#     exit 1
+
+#     fi
