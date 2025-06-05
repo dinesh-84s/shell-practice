cc26a1c8df6444f2785158f5d4079326c6f86e1f
dineshkonda <dineshkonda3239@gmail.com>
2025-06-05 22:45:52 +0530

    hello world started

--- 10-installing-mysql.sh
+++ 10-installing-mysql.sh
@@ -6,7 +6,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: please run the script with using root access"
-    ecit 1 #give other than 0 upto to 127
+    exit 1 #give other than 0 upto to 127
 else 
     echo "you are running with the root access"
 fi
