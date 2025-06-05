6ac2ca3bcf10cad55cdd2f84c250e95e409c0c63
dineshkonda <dineshkonda3239@gmail.com>
2025-06-05 15:13:53 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,3 +4,16 @@
 echo "All variables pass at once: $@"
 
 echo "Number of variables: $#"
+
+echo "Present workind directory $PWD"
+
+echo "PID of the script $$"
+sleep 10 &
+
+echo "script name $0"
+
+echo "User running in this script $USER"
+
+echo "Home directory of the user $HOME"
+
+echo PID of the last command in backround $!"
