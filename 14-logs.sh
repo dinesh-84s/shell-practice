4f3caaa119484bdb6bd8fe6cf033d76ac7b948be
dineshkonda <dineshkonda3239@gmail.com>
2025-06-09 23:47:01 +0530

    hello world started

--- 14-logs.sh
+++ 14-logs.sh
@@ -11,53 +11,54 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 b  
 mkdir -p $LOGS_FOLDER
-echo "statred and execute the skript at with: $(date)" | teen -a $LOG_FILE
+echo "statred and execute the skript at with: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo -e $R "ERROR:: please run the script with using root access $N" | teen -a $LOG_FILE
+    echo -e $R "ERROR:: please run the script with using root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto to 127
 else 
-    echo "you are running with the root access" | teen -a $LOG_FILE
+    echo "you are running with the root access" | tee -a $LOG_FILE
 fi
 
 #vaildate functions takes input as exit status what command they tried to install
 VALIDATE(){
 if [ $1 -eq 0 ]
 then
-    echo "Installing $2 is... $G SUCCESS $N" | teen -a $LOG_FILE
+    echo "Installing $2 is... $G SUCCESS $N" | tee -a $LOG_FILE
 else
-    echo "Installing $2 is... $R FAILURE $N" | teen -a $LOG_FILE
+    echo "Installing $2 is... $R FAILURE $N" | tee -a $LOG_FILE
     exit 1
 fi
 }
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>>&LOG_FILE
 if [ $? -ne 0 ]
 then 
 
-    echo "Mysql not installaed is going to be.. installed" | teen -a $LOG_FILE
-     VALIDATE $? "Mysql"
+    echo "Mysql not installaed is going to be.. installed" | tee -a $LOG_FILE
+    dnf install mysql -y &>>LOG_FILE
+    VALIDATE $? "Mysql"
 else 
-    echo -e "Nothing  to do mysql.... $Y is already installed $N" | teen -a $LOG_FILE
+    echo -e "Nothing  to do mysql.... $Y is already installed $N" | tee -a $LOG_FILE
 fi 
 
 dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo "python3 not installaed is going to be.. installed" | teen -a $LOG_FILE
-    dnf install python3 -y
+    echo "python3 not installaed is going to be.. installed" | tee -a $LOG_FILE
+    dnf install python3 -y &>>&LOG_FILE
     VALIDATE $? "python3"
 else 
-    echo -e "Nothing to do Python3.... $Y is already installed $N" | teen -a $LOG_FILE
+    echo -e "Nothing to do Python3.... $Y is already installed $N" | tee -a $LOG_FILE
 fi 
 
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo "nginx not installaed is going to be.. installed" | teen -a $LOG_FILE
-    dnf install nginx -y
+    echo "nginx not installaed is going to be.. installed" | tee -a $LOG_FILE
+    dnf install nginx -y &>>&LOG_FILE
     VALIDATE $? "nginx"
 else 
-    echo -e "Nothing to do nginx.... $Y is already installed $N" | teen -a $LOG_FILE
+    echo -e "Nothing to do nginx.... $Y is already installed $N" | tee -a $LOG_FILE
 fi 
