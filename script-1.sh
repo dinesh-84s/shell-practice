4ed73f72c43a57127383810f5e2ef8e0ea306408
dineshkonda <dineshkonda3239@gmail.com>
2025-07-04 17:47:04 +0530

    hello-worls-started

---
+++ script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GREETING="Hello World DevOps"
+echo "$GREETING"
+echo "PID of SCRIPT-2 $$"
+
+./script-2.sh
---
+++ script-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "PID of SCRIPT-1 $$"
+
+echo "$GREETINGS"
