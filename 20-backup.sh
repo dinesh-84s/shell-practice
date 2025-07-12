8893e99bc06c5ac50ffb02e59ae3c4c1d382c859
dineshkonda <dineshkonda3239@gmail.com>
2025-07-12 16:38:18 +0530

    hello-world-started

--- 20-backup.sh
+++ 20-backup.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/backup.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SOURCE_DIR=/home/ec2-user/app-logs
-
-mkdir -p $LOGS_FOLDER
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
@@ -30,3 +23,38 @@ VALIDATE(){
         exit 1
     fi
 }
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" 
+    fi
+}
+
+check_root
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
+    exit 1
+fi
