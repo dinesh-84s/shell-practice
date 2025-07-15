1708f96c4754492ca33b91a452724539dc0ecfd4
dineshkonda <dineshkonda3239@gmail.com>
2025-07-15 16:15:44 +0530

    hello-world-started

--- 20-backup.sh
+++ 20-backup.sh
@@ -64,6 +64,9 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 if [ ! -z $FILES ]
 then
     echo "Files to zip are: $FILES"
+    TIME_STAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE="$DEST_DIR/app-logs-$TIME_STAMP.zip"
+    echo $FILES | zip -@ $ZIP_FILE
 else
     echo -e "No log files found older than 14days... $Y SKIPPING $N"
 fi
