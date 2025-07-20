0fa39fc4efcab24d1066eb4624aae33577ba1dc7
dineshkonda <dineshkonda3239@gmail.com>
2025-07-20 16:10:35 +0530

    hello-world-started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -6,6 +6,6 @@ DISK_THRESHOLD=1 # in project it will be 75
 while IFS= read line 
 do
     USAGE=$(echo $line | awk {print '6F'} | cut -d '%' -f1)
-    PARTITION=$(echo $line | awk {print '7F'}
+    PARTITION=$(echo $line | awk {print '7F'})
     echo "$PARTITION: $USAGE"
 done <<< $DISK_USAGE
