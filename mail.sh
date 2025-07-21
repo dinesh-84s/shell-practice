6a35ff956d9eb05942b34bdc6170aa3f0438cde7
dineshkonda <dineshkonda3239@gmail.com>
2025-07-21 15:23:22 +0530

    hello-world-started

--- mail.sh
+++ mail.sh
@@ -9,7 +9,7 @@ TO_ADDRESS=$5
 SUBJECT=$6
 
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$MSG/g")
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG/g")
 {
 echo "To: $TO_ADDress"
 echo "Subject: $SUBJECT"
